7b6ca58501ab1e6d11afbe1cab79d083440b8058
ncarr6 <nancy.l.carr2@gmail.com>
2026-09-07 13:13:31 -0600

    start navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 const links = ["Dashboard", "Transactions", "Accounts", "Settings"];
 import { FaMoneyBills } from "react-icons/fa6";
+import { CatAvatar } from "../assets/index.js"
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 function Navbar() {
 
@@ -17,18 +19,41 @@ function Navbar() {
         <span className="text-xl font-bold text-black dark:text-white">My Spending</span>
       </div>
 
-      {/* hidden on small devices */}
+      {/* links hidden on small devices  */}
       <div className="hidden md:flex items-center gap-4">
         {links.map((link, index) => (
           <div key={index} className={`${index === selected ?  "bg-black dark:bg-slate-800 text-white" 
             : "text-gray-700 dark:text-grey-500"} px-6 py-2 rounded-full`}
-            
+            onClick={()=> setSelected(index)}
             >
             <a href="#">{link}</a>
           </div>
 
         ))}  
       </div>
+
+      <div className="flex items-center gap-10 2xl:gap-20">
+        { /* <ThemeSwitch/> */}
+
+        <div className="flex items-center gap-2">
+         
+          <img src={CatAvatar} alt="user image"
+          className="w-10 md:w-12 h-10 md:h-12
+          rounded-full object-cover cursor-pointer"/>
+
+          <div className="hidden md:block">
+            <p className="text-lg font-medium text-black dark:text-gray-400">User Name!</p>
+            <span className="text-sm text-gray-700 dark:text-gray-500">user@email.com</span>
+          </div>
+
+          <MdOutlineKeyboardArrowDown className="hidden md:block text-2xl
+          text-gray-600 dark:text-gray-300 cursor-pointer"/>
+          
+        </div>
+      </div>
+
+
+
     </div>
   )
 
