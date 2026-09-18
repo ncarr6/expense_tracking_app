2885029bf8873e72c2e7ece8a996736ffdd5cf85
ncarr6 <nancy.l.carr2@gmail.com>
2026-09-17 19:46:33 -0600

    creating transaction cmp

--- frontend/src/components/transactions.jsx
+++ frontend/src/components/transactions.jsx
@@ -1,7 +1,26 @@
+import Title from "./title"
+
 function Transactions() {
   
   return (
-    <div>Transactions</div>
+    <div className="py-20 w-full md:w-2/3">
+      
+      <Title title ="Latest Transactions"/>
+
+      <div className="overflow-x-auto mt-5">
+        <table className="w-full">
+          <thead className="w-full border-b border-solid border-gray-800">
+            <tr className="w-full text-black text-left">
+              <th className="py-2">Date</th>
+              <th className="py-2">Name</th>
+              <th className="py-2">Amount</th>
+            </tr>
+          </thead>
+
+        </table>
+
+      </div>
+    </div>
   )
 
 }
