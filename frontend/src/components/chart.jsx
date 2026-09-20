59f4dfba9f666c1b1a3b00c7835c86e34e1726fd
ncarr6 <nancy.l.carr2@gmail.com>
2026-09-20 12:57:53 -0600

    Adjust window size

--- frontend/src/components/chart.jsx
+++ frontend/src/components/chart.jsx
@@ -19,7 +19,7 @@ function Chart() {
       <ResponsiveContainer width={"100%"} height={500} className="mt-5">
         <LineChart width={500} height={300} data={test_data}>
           <CartesianGrid strokeDasharray='3 3'/>
-          <YAxis />
+          <YAxis domain={[0, dataMax => dataMax + 500]}/>
           <XAxis 
             dataKey="label" 
             type="category" 
